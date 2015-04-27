2a3b1a1038cdd2a4dd3245157c513731baced547
Lin <linsara1@gmail.com>
2015-04-27 18:29:39 +0200

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,51 +14,51 @@ puts("# ----------------------- Players ----------------------- ")
 	p.save!
 
 puts("# ----------------------- Validations ----------------------- ")
-	v = Validation.new(user_digitization:'بشيء',word_image_url: 'assets/29.jpg',
+	v = Validation.new(user_digitization:'بشيء',word_image_url: "/assets/29.jpg",
 			word_id:'1', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'لم',word_image_url: 'assets/30.jpg',
+	v = Validation.new(user_digitization:'لم',word_image_url: '/assets/30.jpg',
 			word_id:'2', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'على',word_image_url: 'assets/40.jpg',
+	v = Validation.new(user_digitization:'على',word_image_url: '/assets/40.jpg',
 			word_id:'3', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'اجتمعوا',word_image_url: 'assets/39.jpg',
+	v = Validation.new(user_digitization:'اجتمعوا',word_image_url: '/assets/39.jpg',
 			word_id:'4', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'ولو',word_image_url: 'assets/38.jpg',
+	v = Validation.new(user_digitization:'ولو',word_image_url: '/assets/38.jpg',
 			word_id:'5', certinity_rate:'1',known:'false')
 	v.save!
 	
-	v = Validation.new(user_digitization:'لك',word_image_url: 'assets/37.jpg',
+	v = Validation.new(user_digitization:'لك',word_image_url: '/assets/37.jpg',
 			word_id:'6', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'الله',word_image_url: 'assets/36.jpg',
+	v = Validation.new(user_digitization:'الله',word_image_url: '/assets/36.jpg',
 			word_id:'7', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'كتبه',word_image_url: 'assets/35.jpg',
+	v = Validation.new(user_digitization:'كتبه',word_image_url: '/assets/35.jpg',
 			word_id:'8', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'قد',word_image_url: 'assets/34.jpg',
+	v = Validation.new(user_digitization:'قد',word_image_url: '/assets/34.jpg',
 			word_id:'9', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'بشيء',word_image_url: 'assets/33.jpg',
+	v = Validation.new(user_digitization:'بشيء',word_image_url: '/assets/33.jpg',
 			word_id:'10', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'إلا',word_image_url: 'assets/32.jpg',
+	v = Validation.new(user_digitization:'إلا',word_image_url: '/assets/32.jpg',
 			word_id:'11', certinity_rate:'1',known:'false')
 	v.save!
 
-	v = Validation.new(user_digitization:'ينفعوك',word_image_url: 'assets/31.jpg',
+	v = Validation.new(user_digitization:'ينفعوك',word_image_url: '/assets/31.jpg',
 			word_id:'12', certinity_rate:'1',known:'false')
 	v.save!
 
